87c72fe6066514073d0c10a79e29582df0225917
John Gibson <jg20019@gmail.com>
2023-05-19 16:16:46 -0400

    feat: Remove unused components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,8 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-    <h1 class="text-2xl font-bold"> Hello World </h1>
+    <h1> Learning C# </h1>
 </main>
 
 <style>
