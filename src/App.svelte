2207fdd42b6323d5267a02c4b4726b90ee20b355
John Gibson <jg20019@gmail.com>
2023-05-19 16:45:30 -0400

    feat(wip): Add componet to view topics

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,34 @@
 <script>
-import Layout from './lib/Layout.svelte';
 import Home from './lib/Home.svelte';
+import Layout from './lib/Layout.svelte';
+import TopicDetail from './lib/TopicDetail.svelte';
 
-let view = 'Learning C#'
+let topicName = 'Learning C#'
+let topics = [
+    { 
+      name: 'Learning C#',
+      entries: [
+        {date: '2023-05-01', minutes: 85,  desc: 'Introduction to C#'},
+        {date: '2023-05-02', minutes: 90,  desc: 'Making a CLI'},
+        {date: '2023-05-03', minutes: 120, desc: 'Making Quick Block'},
+        {date: '2023-05-12', minutes: 30,  desc: 'DNS in a Weekend'},
+        {date: '2023-05-13', minutes: 45,  desc: 'DNS in a Weekend'},
+        {date: '2023-05-13', minutes: 30,  desc: 'DNS in a Weekend'},
+        {date: '2023-05-14', minutes: 45,  desc: 'DNS in a Weekend'},
+      ]
+    }
+]
 
+$: currentTopic = topics.find(({name}) => topicName === name)
 
 </script>
 
 <main>
-    <Layout on:go-home={() => view = 'home'}> 
-        {#if view != "home" }
-            <h1> { view } </h1>
-        {:else}
+    <Layout on:go-home={() => topicName = null }> 
+        {#if topicName === null }
             <Home /> 
+        {:else}
+            <TopicDetail topic={currentTopic} />
         {/if}
     </Layout>
 </main>
---
+++ src/lib/TopicDetail.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let topic = null
+</script>
+
+{#if topic == null }
+    <h1> Topic not found </h1>
+{:else }
+    <h1> { topic.name } </h1>
+{/if}
