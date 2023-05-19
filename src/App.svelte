5e2a320affd8814a68254ebfd2f644c238a57670
John Gibson <jg20019@gmail.com>
2023-05-19 16:14:52 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+    <h1 class="text-2xl font-bold"> Hello World </h1>
+</main>
+
+<style>
+</style>
